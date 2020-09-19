7f5dde155372a18391195b9631e3af888b262959
Casper Guldbech Nielsen <whopsec@protonmail.com>
2020-09-19 12:06:14 +0200

    Removing redundant info
    Signed-off-by: Casper Guldbech Nielsen <whopsec@protonmail.com>

--- ade/connectors/connectors.py
+++ ade/connectors/connectors.py
@@ -13,7 +13,7 @@ class Connectors():
     def ldap_connector(self, server: str, ldaps: bool, domuser: str, passwd: str, level='ALL') -> ldap3.Connection:
         '''Returns an ldap3.Connection object that is bound to the supplied domain controller
 
-        Raise ldap3.core.exceptions.LDAPBindError on bind() errors.
+        Raise LDAPBindError on bind() errors.
 
         '''
         if ldaps:
@@ -30,7 +30,6 @@ class Connectors():
             conn.bind()
             # Validate the login (bind) request
             if int(conn.result['result']) != 0:
-                print('\033[1A\r[ ' + colored('ERROR', 'red') +' ] Failed to bind to LDAP server: {0}'.format(conn.result['description']))
                 raise LDAPBindError
         
         return conn
